eefaab6704957f4f7d7fcc81042040d097fb3390
Aaron Peckham <apeckham@gmail.com>
2013-04-23 16:22:03 -0400

    update to accommodate tumblr api changes; make config_file optional; add requires

--- lib/tumbleweed.rb
+++ lib/tumbleweed.rb
@@ -1,23 +1,23 @@
 require "tumbleweed/version"
+require "mechanize"
+require "json"
 
 module Tumbleweed
-  def self.upload_theme blog, user, password, theme_file, config_file
-    url = 'https://www.tumblr.com'
-
+  def self.upload_theme blog, email, password, theme_file, config_file = nil
     agent = Mechanize.new
-    page = agent.get(url)
+    page = agent.get "https://www.tumblr.com/login"
 
-    # Login
-    login_form = page.forms[1]
-    login_form.email = email
-    login_form.password = password
+    login_form = page.forms.first
+    login_form.action = "/login"
+    login_form["user[email]"] = email
+    login_form["user[password]"] = password
     agent.submit(login_form)
 
     page = agent.get("http://www.tumblr.com/customize/#{blog}")
     user_form_key = page.body.scan(/Tumblr\.Customize\.user_form_key\s*=\s*'([^']+)';/).flatten.first
 
     theme = File.read(theme_file)
-    config = YAML.load_file(config_file)
+    config = config_file ? YAML.load_file(config_file) : {}
 
     json = config.merge(
       'user_form_key' => user_form_key,
