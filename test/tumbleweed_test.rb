4a53b4481a2a9023503d59991d3d4d80546e9dd6
Aaron Peckham <apeckham@gmail.com>
2013-06-18 11:20:17 -0700

    fix tumbleweed; tumblr changed its javascript and the regex had to be updated

--- test/tumbleweed_test.rb
+++ test/tumbleweed_test.rb
@@ -7,14 +7,14 @@ require File.dirname(__FILE__) + "/../lib/tumbleweed"
 class Test::Unit::TestCase
   def test_upload_theme
     assert_equal 3, [ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD']].compact.length, "BLOG, EMAIL and PASSWORD are required"
-    random_string = rand.to_s
-    
+    template_string = [Time.now, rand].to_s
+
     theme_file = Tempfile.new(__FILE__)
-    theme_file << random_string
+    theme_file << template_string
     theme_file.flush
     Tumbleweed.upload_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD'], theme_file.path
     
     actual = open("http://#{ENV['BLOG']}.tumblr.com/?#{rand}").read
-    assert_include actual, random_string
+    assert_include actual, template_string
   end
 end
