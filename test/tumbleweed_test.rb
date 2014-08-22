1a646cd638453ddee5d31cca9b42ab5d72186ac1
apeckham <apeckham@gmail.com>
2014-08-22 09:45:05 -0700

    refactor spec

--- test/tumbleweed_test.rb
+++ test/tumbleweed_test.rb
@@ -7,29 +7,25 @@ require File.dirname(__FILE__) + "/../lib/tumbleweed"
 describe Tumbleweed do
   before do
     assert_equal 3, [ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD']].compact.length, "BLOG, EMAIL and PASSWORD are required"
+
+    @unique_string = [Time.now, rand].to_s
+
+    @theme_file = Tempfile.new('tumbleweed')
+    @theme_file << @unique_string
+    @theme_file.flush
   end
 
   it "uploads a theme" do
-    unique_string = [Time.now, rand].to_s
-
-    theme_file = Tempfile.new('tumbleweed')
-    theme_file << unique_string
-    theme_file.flush
-    Tumbleweed.upload_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD'], theme_file.path
+    Tumbleweed.upload_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD'], @theme_file.path
     
     actual = open("http://#{ENV['BLOG']}.tumblr.com/?#{rand}").read
-    actual.must_include unique_string
+    actual.must_include @unique_string
   end
 
   it "downloads a theme" do
-    unique_string = [Time.now, rand].to_s
-
-    theme_file = Tempfile.new('tumbleweed')
-    theme_file << unique_string
-    theme_file.flush
-    Tumbleweed.upload_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD'], theme_file.path
+    Tumbleweed.upload_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD'], @theme_file.path
 
     actual = Tumbleweed.download_theme ENV['BLOG'], ENV['EMAIL'], ENV['PASSWORD']
-    actual.must_include unique_string
+    actual.must_include @unique_string
   end
 end
