238b4d976f613a0fe8d9c1112d67526b023dce1c
apeckham <apeckham@gmail.com>
2014-08-22 09:52:31 -0700

    refactor

--- lib/tumbleweed.rb
+++ lib/tumbleweed.rb
@@ -5,46 +5,56 @@ require 'logger'
 
 module Tumbleweed
   LOGIN_URL = 'https://www.tumblr.com/login'
+  
+  class Client
+    attr_reader :agent
+    
+    def initialize
+      @agent = Mechanize.new
+      @agent.log = Logger.new(STDERR) if ENV['DEBUG']
+    end
+
+    def login(email, password)
+      page = @agent.get LOGIN_URL
+      form = page.forms.find { |form| form.action == LOGIN_URL }
+      form['user[email]'] = email
+      form['user[password]'] = password
+      @agent.submit(form)
+      raise 'login failed' unless @agent.cookies.find { |cookie| cookie.name == 'logged_in' }
+    end
+
+    def customize(blog_name)
+      page = @agent.get("https://www.tumblr.com/customize/#{blog_name}")
+      body = page.body
+
+      blog_json = JSON.parse(body[/Tumblr\._init\.blog = (\{[^\n]+\});/, 1])
+      [body, blog_json]
+    end
+
+    def post(blog_name, json)
+      @agent.post("https://www.tumblr.com/customize_api/blog/#{blog_name}", json)
+    end
+  end
 
   def self.upload_theme blog_name, email, password, theme_file, config_file = nil
-    agent = Mechanize.new
-    agent.log = Logger.new(STDERR) if ENV['DEBUG']
-    page = agent.get LOGIN_URL
+    client = Client.new
+    client.login(email, password)
 
-    form = page.forms.find { |form| form.action == LOGIN_URL }
-    form['user[email]'] = email
-    form['user[password]'] = password
-    agent.submit(form)
-    raise 'login failed' unless agent.cookies.find { |cookie| cookie.name == 'logged_in' }
-
-    page = agent.get("https://www.tumblr.com/customize/#{blog_name}")
-    body = page.body
-
-    blog = JSON.parse(body[/Tumblr\._init\.blog = (\{[^\n]+\});/, 1])
+    body, blog = client.customize(blog_name)
     blog.reverse_merge(YAML.load_file(config_file)) if config_file
     blog['user_form_key'] = body[/Tumblr\._init\.user_form_key = '([^\n]+)';/, 1]
     blog['secure_form_key'] = body[/Tumblr\._init\.secure_form_key = '([^\n]+)';/, 1]
     blog['custom_theme'] = theme_file == '-' ? STDIN.read : File.read(theme_file)
     blog['id'] = blog_name
 
-    agent.post("https://www.tumblr.com/customize_api/blog/#{blog_name}", blog.to_json)
+    client.post(blog_name, blog.to_json)
   end
 
   def self.download_theme blog_name, email, password
-    agent = Mechanize.new
-    agent.log = Logger.new(STDERR) if ENV['DEBUG']
-    page = agent.get LOGIN_URL
+    client = Client.new
+    client.login(email, password)
 
-    form = page.forms.find { |form| form.action == LOGIN_URL }
-    form['user[email]'] = email
-    form['user[password]'] = password
-    agent.submit(form)
-    raise 'login failed' unless agent.cookies.find { |cookie| cookie.name == 'logged_in' }
-
-    page = agent.get("https://www.tumblr.com/customize/#{blog_name}")
-    body = page.body
-
-    blog = JSON.parse(body[/Tumblr\._init\.blog = (\{[^\n]+\});/, 1])
-    blog['custom_theme']
+    body, blog_json = client.customize(blog_name)
+    blog_json['custom_theme']
   end
 end
